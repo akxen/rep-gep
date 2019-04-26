ff913f24eb70c6486beb200c0da4155916afad08
Aleksis Xenophon <aleksisx@eee.hku.hk>
2019-04-26 15:11:53 +0800

    (Update) Padding beginning and end of selected datasets. Refactoring k-means implementation, and using standardisation.

--- src/2_input_traces/combine_traces.py
+++ src/2_input_traces/combine_traces.py
@@ -3,6 +3,110 @@
 import os
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def pad_dates_backward(df):
+    """
+    Pad beginning of DataFrame with values corresponding to the closest day-hour
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        DataFrame with timestamp index
+
+    Returns
+    -------
+    df_o : pandas DataFrame
+        DataFrame with padded values at beginning of time index
+    """
+
+    # Check for missing values
+    assert not df.isna().any().any(), 'Missing demand values'
+
+    # Backward fill using the first available observation for corresponding day-hour
+    # ------------------------------------------------------------------------------
+    df_tmp = df.copy()
+    df_tmp[('INDEX', 'MINUS_1_YEAR')] = df_tmp.apply(lambda x: x.name - pd.Timedelta(days=365), axis=1)
+    df_tmp = df_tmp.set_index(('INDEX', 'MINUS_1_YEAR'))
+
+    # New index for beginning of demand data
+    new_index_start = pd.date_range(start='2016-01-01 01:00:00', end=df.index[-1], freq='1H')
+
+    # Reindex DataFrame
+    df_o = df.reindex(new_index_start).copy()
+
+    # Update missing value at start of index with corresponding day-hours from following year
+    df_o.update(df_tmp, overwrite=False)
+
+    return df_o
+
+
+def pad_dates_forward(df):
+    """
+    Pad end of DataFrame with values corresponding to the closest day-hour
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        DataFrame with timestamp index
+
+    Returns
+    -------
+    df_o : pandas DataFrame
+        DataFrame with padded values at end of time index
+    """
+
+    # Data for last year in dataset (include 366 days in case of leap year)
+    df_last_year = df[-8784:].copy()
+
+    # Add index for day and hour of year
+    df_last_year[('INDEX', 'DAY_OF_YEAR')] = df_last_year.index.dayofyear
+    df_last_year[('INDEX', 'HOUR')] = df_last_year.index.hour
+    df_last_year = df_last_year.set_index([('INDEX', 'DAY_OF_YEAR'), ('INDEX', 'HOUR')])
+
+    # Drop duplicates (if no leap year some rows will be removed)
+    df_last_year = df_last_year[~df_last_year.index.duplicated(keep='last')]
+
+    # In the event of a leap year, assume that the 366th day of the year is similar to the 365th
+    if 366 not in df_last_year.index.levels[0]:
+        leap_day = df_last_year.loc[365].copy()
+        leap_day.loc[:, ('INDEX', 'DAY')] = 366
+        leap_day = leap_day.set_index(('INDEX', 'DAY'), append=True).swaplevel(1, 0, 0)
+
+        # Update last year data with leap day
+        df_last_year = pd.concat([df_last_year, leap_day])
+
+    # Assert no duplicates in index
+    assert not df_last_year.index.duplicated().any(), 'Duplicated day-hour index'
+
+    # Check no missing values before updating (don't want to mistakenly update other years)
+    assert not df.isna().any().any(), 'Missing values'
+
+    # New index for end of DataFrame
+    new_index_end = pd.date_range(start=df.index[0], end='2050-01-01 00:00:00', freq='1H')
+
+    # Reindex DataFrame
+    df_o = df.reindex(new_index_end).copy()
+
+    # Add timestamp, day of year, and hour to DataFrame
+    df_o[('INDEX', 'TIMESTAMP')] = df_o.index
+    df_o[('INDEX', 'DAY_OF_YEAR')] = df_o.index.dayofyear
+    df_o[('INDEX', 'HOUR')] = df_o.index.hour
+
+    # Set index as day of year and hour
+    df_o = df_o.set_index([('INDEX', 'DAY_OF_YEAR'), ('INDEX', 'HOUR')])
+
+    # Update data for final years in dataset
+    df_o.update(df_last_year, overwrite=False)
+
+    # Use timestamp as index
+    df_o = df_o.set_index(('INDEX', 'TIMESTAMP'))
+
+    # Check no missing values
+    # assert not df_o.isna().any().any(), 'Missing values'
+
+    return df_o
 
 
 def format_wind_traces(data_dir):
@@ -16,6 +120,9 @@ def format_wind_traces(data_dir):
 
     # Add level to column index
     df.columns = pd.MultiIndex.from_product([['WIND'], df.columns])
+
+    # Pad data forward
+    df = pad_dates_forward(df)
 
     return df
 
@@ -61,6 +168,12 @@ def format_demand_traces(data_dir, network_dir):
     # Add label to columns
     df_zone_demand.columns = pd.MultiIndex.from_product([['DEMAND'], df_zone_demand.columns])
 
+    # Backfill corresponding day-hour values to extend the series
+    df_zone_demand = pad_dates_backward(df_zone_demand)
+
+    # Forwardfill corresponding day-hour values to extend the series
+    df_zone_demand = pad_dates_forward(df_zone_demand)
+
     return df_zone_demand
 
 
@@ -79,6 +192,12 @@ def format_solar_traces(data_dir):
 
     # Add column index denoting solar data
     df.columns = pd.MultiIndex.from_product([['SOLAR'], df.columns])
+
+    # Pad dates backward (ensure data starts from 2016)
+    df = pad_dates_backward(df)
+
+    # Pad dates forward (ensure data ends at beginning of 2050
+    df = pad_dates_forward(df)
 
     return df
 
@@ -145,84 +264,47 @@ if __name__ == '__main__':
     # Formatted traces
     # ----------------
     # Wind traces
-    # df_wind = format_wind_traces(output_directory)
+    df_wind = format_wind_traces(output_directory)
 
     # Demand traces
     df_demand = format_demand_traces(output_directory, network_data_directory)
 
+    # Hydro traces
+    df_hydro = format_hydro_traces(output_directory)
+
+    # Solar traces
+    df_solar = format_solar_traces(output_directory)
+
+    # Merge into single DataFrame
+    # ---------------------------
+    # Join datasets
+    df_dataset = df_hydro.join(df_wind, how='left').join(df_demand, how='left').join(df_solar, how='left')
+
     # Check for missing values
-    assert not df_demand.isna().any().any(), 'Missing demand values'
+    assert not df_dataset.isna().any().any(), 'Missing values in dataset'
 
-    df_tmp = df_demand.copy()
-    df_tmp[('INDEX', 'MINUS_1_YEAR')] = df_tmp.apply(lambda x: x.name - pd.Timedelta(days=365), axis=1)
-    df_tmp = df_tmp.set_index(('INDEX', 'MINUS_1_YEAR'))
+    # Check for duplicated indices
+    assert not df_dataset.index.duplicated().any(), 'Dataset has duplicated indices'
 
-    # New index for beginning of demand data
-    new_index_start = pd.date_range(start='2016-01-01 01:00:00', end=df_demand.index[-1], freq='1H')
+    # Check for duplicated columns
+    assert not df_dataset.columns.duplicated().any(), 'Dataset has duplicated columns'
 
-    # Reindex DataFrame
-    df_demand = df_demand.reindex(new_index_start)
+    # Plots of selected series to double check data to double check
+    # -------------------------------------------------------------
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4)
 
-    # Update missing value at start of index with corresponding day-hours from following year
-    df_demand.update(df_tmp, overwrite=False)
+    # Demand
+    df_dataset[('DEMAND', 'CAN')].plot(title='Demand - CAN', ax=ax1)
 
-    # Data for last year in dataset
-    df_last_year = df_demand[-8760:].copy()
-    df_last_year[('INDEX', 'DAY_OF_YEAR')] = df_last_year.index.dayofyear
-    df_last_year[('INDEX', 'HOUR')] = df_last_year.index.hour
-    df_last_year = df_last_year.set_index([('INDEX', 'DAY_OF_YEAR'), ('INDEX', 'HOUR')])
+    # Hydro
+    df_dataset[('HYDRO', 'MEADOWBK')].plot(title='Hydro - MEADOWBK', ax=ax2)
 
-    # Assert no duplicates in index
-    assert not df_last_year.index.duplicated().any(), 'Duplicated day-hour index'
+    # Wind
+    df_dataset[('WIND', 'FNQ')].plot(title='Wind - FNQ', ax=ax3)
 
-    # Check no missing values
+    # Solar
+    df_dataset[('SOLAR', 'ADE|FFP2')].plot(title='Solar - ADE|FFP2', ax=ax4)
 
-    # New index for end of DataFrame
-    new_index_end = pd.date_range(start=df_demand.index[0], end='2050-01-01 00:00:00', freq='1H')
-
-    # Reindex DataFrame
-    df_demand = df_demand.reindex(new_index_end)
-
-    # Add timestamp, day of year, and hour to DataFrame
-    df_demand[('INDEX', 'TIMESTAMP')] = df_demand.index
-    df_demand[('INDEX', 'DAY_OF_YEAR')] = df_demand.index.dayofyear
-    df_demand[('INDEX', 'HOUR')] = df_demand.index.hour
-
-    # Set index as day of year and hour
-    df_demand = df_demand.set_index([('INDEX', 'DAY_OF_YEAR'), ('INDEX', 'HOUR')])
-
-
-
-    # Update demand
-    df_demand.update(df_last_year, overwrite=False)
-
-    df_demand = df_demand.set_index(('INDEX', 'TIMESTAMP'))
-    df_demand[('DEMAND', 'CAN')].plot()
-
-
-
-
-
-
-
-
-
-
-
-    #
-    # # Hydro traces
-    # df_hydro = format_hydro_traces(output_directory)
-    #
-    # # Solar traces
-    # df_solar = format_solar_traces(output_directory)
-    #
-    # # Merge into single DataFrame
-    # # ---------------------------
-    # # Join datasets
-    # df_dataset = df_hydro.join(df_wind, how='left').join(df_demand, how='left').join(df_solar, how='left')
-    #
-    # # Add timestamp as a column
-    # df_dataset[('INDEX', 'TIMESTAMP')] = df_dataset.index
-    #
-    # # Save to file
-    # df_dataset.to_hdf(os.path.join(output_directory, 'dataset.h5'), key='dataset')
+    # Save to file
+    # ------------
+    df_dataset.to_hdf(os.path.join(output_directory, 'dataset.h5'), key='dataset')
--- src/2_input_traces/kmeans_scenario_reduction.py
+++ src/2_input_traces/kmeans_scenario_reduction.py
@@ -1,10 +1,11 @@
 """Apply K-mean scenario algorithm to identify representative set of scenarios"""
 
+from math import sqrt
+
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
 
-# import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 
 def get_all_samples(df):
@@ -34,12 +35,44 @@ def get_all_samples(df):
     return df_s
 
 
+def get_samples_max_demand_days(df):
+    """Identify days with greatest demand"""
+
+    # Filter days with max demand in each year
+    max_demand_days = df['DEMAND'].sum(level=[1], axis=1).max(axis=1).groupby(('INDEX', 'YEAR')).idxmax()
+    max_demand_days = pd.MultiIndex.from_tuples(max_demand_days)
+
+    # Samples for days on which max system demand occurs each year
+    df_o = df.loc[max_demand_days, :].copy()
+
+    return df_o
+
+
+def standardise_samples(df):
+    """Standardise samples"""
+
+    # Standardise samples
+    mean = df.mean()
+    std = df.std()
+
+    # If no variation in column, set std = 1 (avoid divide 0 errors). Same
+    # as what sklearn does when scaling.
+    std = std.replace(0, 1)
+
+    # Standardised samples
+    df_o = (df - mean) / std
+
+    # Check that NaNs are not in standardised DataFrame
+    assert not df_o.replace(-np.inf, np.nan).replace(np.inf, np.nan).isna().any().any()
+
+    return df_o
+
+
 def get_centroids(df, year, n_clusters=7):
     """Apply K-means algorithm to compute centroids for a given year"""
 
     # Sample
     x = df.loc[year].values
-    # .fillna(0)
 
     # Construct and fit K-means classifier
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(x)
@@ -69,29 +102,88 @@ if __name__ == '__main__':
     # All samples
     samples = get_all_samples(dataset)
 
-    # Container for all centroids
-    all_centroids = []
+    # Samples for days with max demand
+    df_max_demand = get_samples_max_demand_days(samples)
 
-    for year in range(2016, 2051):
+    # Remaining samples
+    df_s = samples.loc[samples.index.difference(df_max_demand.index), :].copy()
 
-        try:
-            # Compute centroids and associated duration for each sample
-            centroids = get_centroids(samples, year)
+    # Scale sample for one year
+    df = df_s.loc[2016, :].copy()
 
-            # Add year to index
-            centroids['year'] = year
-            centroids = centroids.set_index('year', append=True).swaplevel(1, 0, 0)
+    # Standardise samples for a given year
+    df_in = standardise_samples(df)
 
-            # Append to main container
-            all_centroids.append(centroids)
+    # Samples to be used in K-mean classifier
+    X = df_in.values
 
-            print(f'Finished processing centroids for {year}')
+    # Construct and fit K-means classifier
+    n_clusters = 9
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
 
-        except:
-            print(f'Failed to process year {year}')
+    # Assign each sample to a cluster
+    X_pred = kmeans.predict(X)
 
-    # Combine all centroids into single DataFrame
-    df_c = pd.concat(all_centroids)
+    # Assign cluster ID to each sample
+    df_in[('K_MEANS', 'METRIC', 'CLUSTER')] = X_pred
+    df_in[('K_MEANS', 'METRIC', 'CLUSTER')] = df_in[('K_MEANS', 'METRIC', 'CLUSTER')].astype(int)
 
-    # Save output
-    df_c.to_pickle('output/centroids.pickle')
+    # df_in.apply(lambda x: kmeans.cluster_centers_[int(x[('K_MEANS', 'METRIC', 'CLUSTER')])], axis=1)
+
+    def _compute_cluster_distance(row):
+        """Compute distance between sample and it's assigned centroid"""
+
+        # Get centroid
+        centroid = kmeans.cluster_centers_[int(row[('K_MEANS', 'METRIC', 'CLUSTER')])]
+
+        # Sample (removing cluster number information)
+        sample = row.drop(('K_MEANS', 'METRIC', 'CLUSTER'))
+
+        # Euclidean norm between sample and centroid
+        distance = np.linalg.norm(sample - centroid)
+
+        return distance
+
+    # Compute Euclidean norm between each sample and its respective centroid
+    df_in[('K_MEANS', 'METRIC', 'CENTROID_DISTANCE')] = df_in.apply(_compute_cluster_distance, axis=1)
+
+    # Find samples that minimise distance to each respective centroid
+    centroid_samples = df_in.groupby(('K_MEANS', 'METRIC', 'CLUSTER'))[[('K_MEANS', 'METRIC', 'CENTROID_DISTANCE')]].idxmin()
+
+    # Reset index
+    centroid_samples = centroid_samples.reset_index()
+
+    a = centroid_samples.reset_index().droplevel(0, axis=1).droplevel(0, axis=1)
+
+    b = pd.merge(a, df_in, how='left', left_on=['CENTROID_DISTANCE'], right_index=True)
+
+
+
+
+
+    # # Container for all centroids
+    # all_centroids = []
+    #
+    # for year in range(2016, 2051):
+    #
+    #     try:
+    #         # Compute centroids and associated duration for each sample
+    #         centroids = get_centroids(samples, year)
+    #
+    #         # Add year to index
+    #         centroids['year'] = year
+    #         centroids = centroids.set_index('year', append=True).swaplevel(1, 0, 0)
+    #
+    #         # Append to main container
+    #         all_centroids.append(centroids)
+    #
+    #         print(f'Finished processing centroids for {year}')
+    #
+    #     except:
+    #         print(f'Failed to process year {year}')
+    #
+    # # Combine all centroids into single DataFrame
+    # df_c = pd.concat(all_centroids)
+    #
+    # # Save output
+    # df_c.to_pickle('output/centroids.pickle')
