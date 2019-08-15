cd05abbe43732ebf46615d9059362b5716176c95
Aleksis <aleksis.git@gmail.com>
2019-08-15 13:27:27 +0800

    (Update) About to refactor directory structure

--- src/3_model/linear/components/base/components.py
+++ src/3_model/linear/components/base/components.py
@@ -86,7 +86,7 @@ class CommonComponents:
         m.G = m.G_E.union(m.G_C)
 
         # All years in model horizon
-        m.Y = RangeSet(2016, 2050)
+        m.Y = RangeSet(2016, 2018)
 
         # Operating scenarios for each year
         m.S = RangeSet(1, 10)
