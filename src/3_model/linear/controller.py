6a31d877a242b7354e73017c3081382765de4ebd
Aleksis <aleksis.git@gmail.com>
2019-09-30 09:11:38 +0800

    (Update) Setting up model to be run on remote

--- src/3_model/linear/controller.py
+++ src/3_model/linear/controller.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     log_file_name = 'case_logger'
-    output_directory = os.path.join(os.path.dirname(__file__), 'output', 'local')
+    output_directory = os.path.join(os.path.dirname(__file__), 'output', 'remote')
 
     # Object used to run model cases
     cases = ModelCases(output_directory, log_file_name)
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     targets = Targets()
 
     # Common model parameters
-    start, end, scenarios = 2016, 2031, 5
+    start, end, scenarios = 2016, 2040, 5
 
     # Year when scheme transitions to a Refunded Emissions Payment (REP) scheme
     transition_year = 2021
@@ -49,11 +49,11 @@ if __name__ == '__main__':
 
     # Run BAU case
     # r_bau = cases.run_bau_case(start, end, scenarios, output_directory)
-    # cases.run_bau_case(start, end, scenarios, output_directory)
+    cases.run_bau_case(start, end, scenarios, output_directory)
 
     # Run REP case
     # r_rep = cases.run_rep_case(start, end, scenarios, permit_prices_model, output_directory)
-    # cases.run_rep_case(start, end, scenarios, permit_prices_model, output_directory)
+    cases.run_rep_case(start, end, scenarios, permit_prices_model, output_directory)
 
     # Run price case targeting model using MPPDC model - minimise price deviation between successive years
     case_params['mode'] = 'price_change_minimisation'
@@ -64,11 +64,11 @@ if __name__ == '__main__':
     # r_price_change_heuristic = cases.run_price_smoothing_heuristic_case(case_params, output_directory)
     cases.run_price_smoothing_heuristic_case(case_params, output_directory)
 
-    # # Run price targeting model using MPPDC model - minimise price difference relative to BAU prices in start year
-    # case_params['mode'] = 'bau_deviation_minimisation'
-    # # r_bau_change_mppdc = cases.run_price_smoothing_mppdc_case(case_params, output_directory)
-    # cases.run_price_smoothing_mppdc_case(case_params, output_directory)
-    #
-    # # Run price case targeting model using auxiliary model
-    # # r_bau_change_heuristic = cases.run_price_smoothing_heuristic_case(case_params, output_directory)
-    # cases.run_price_smoothing_heuristic_case(case_params, output_directory)
+    # Run price targeting model using MPPDC model - minimise price difference relative to BAU prices in start year
+    case_params['mode'] = 'bau_deviation_minimisation'
+    # r_bau_change_mppdc = cases.run_price_smoothing_mppdc_case(case_params, output_directory)
+    cases.run_price_smoothing_mppdc_case(case_params, output_directory)
+
+    # Run price case targeting model using auxiliary model
+    # r_bau_change_heuristic = cases.run_price_smoothing_heuristic_case(case_params, output_directory)
+    cases.run_price_smoothing_heuristic_case(case_params, output_directory)
